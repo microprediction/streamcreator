5369835fc63d71bc5b8f3499a0460ae3986849d1
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2021-01-07 10:02:22 -0500

    Create set.py

---
+++ set.py
@@ -0,0 +1,19 @@
+import os
+from getjson import getjson
+from microprediction import MicroWriter
+
+write_key = os.environ.get('write_key').    # GitHub action needs to set env variable. You need to create a GitHub secret called WRITE_KEY
+mw = MicroWriter(write_key=write_key)
+assert mw.key_difficulty(mw.write_key)>=12, "You need a key of difficulty 12 to create a stream"
+
+REPOS = ['microprediction','neuralprophet','deepecho']
+
+if __name__=='__main__':
+  # Intended to be run once a day
+  for repo in REPOS:
+     value = getjson('https://pypistats.org/api/packages/microprediction/recent')['data']['last_day']
+     name  = 'downloads_'+repo+'.json'
+     mw.write(name=name,value=value)
+     
+     
+  
